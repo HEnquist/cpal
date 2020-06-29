17d50f27eae69c1f85bb80aaada14c1df2de05e7
Henrik Enquist <henrik.enquist@maxiv.lu.se>
2020-06-29 14:22:32 +0200

    Testing with a queue

--- examples/beep.rs
+++ examples/beep.rs
@@ -2,6 +2,8 @@ extern crate anyhow;
 extern crate cpal;
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use std::collections::VecDeque;
+use std::sync::{Arc, Mutex};
 
 fn main() -> Result<(), anyhow::Error> {
     let host = cpal::default_host();
@@ -23,40 +25,63 @@ fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyh
 where
     T: cpal::Sample,
 {
-    let sample_rate = config.sample_rate.0 as f32;
+    //let sample_rate = config.sample_rate.0 as f32;
     let channels = config.channels as usize;
 
-    // Produce a sinusoid of maximum amplitude.
-    let mut sample_clock = 0f32;
-    let mut next_value = move || {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
-    };
+    let vector: Arc<Mutex<VecDeque<f32>>> = Arc::new(Mutex::new(VecDeque::with_capacity(1000000)));
 
     let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
 
+    let vec_cloned = vector.clone();
     let stream = device.build_output_stream(
         config,
         move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-            write_data(data, channels, &mut next_value)
+            write_data(data, channels, &vec_cloned)
         },
         err_fn,
     )?;
+    println!("go!");
     stream.play()?;
+    if let Ok(mut queue) = vector.lock() {
+        fill_queue(&mut queue, 2048);
+    }
 
-    std::thread::sleep(std::time::Duration::from_millis(1000));
-
+    for _t in 0..1000 {
+        std::thread::sleep(std::time::Duration::from_millis(1));
+        if let Ok(mut queue) = vector.lock() {
+            while queue.len() < 2048 {
+                //println!("filling, len is {}", queue.len());
+                fill_queue(&mut queue, 1024);
+            }
+        }
+    }
     Ok(())
 }
 
-fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+fn fill_queue(sharedqueue: &mut VecDeque<f32>, n: usize) {
+    for n in 0..n {
+        let val = (n as f32 / 1024.0 * 10.0 * 2.0 * 3.141592).sin()*0.25;
+        sharedqueue.push_back(cpal::Sample::from::<f32>(&val));
+    }
+} 
+
+fn write_data<T>(output: &mut [T], channels: usize, sharedqueue: &Arc<Mutex<VecDeque<f32>>>)
 where
     T: cpal::Sample,
 {
-    for frame in output.chunks_mut(channels) {
-        let value: T = cpal::Sample::from::<f32>(&next_sample());
-        for sample in frame.iter_mut() {
-            *sample = value;
+    //println!("New buffer, length {}", output.len());
+    if let Ok(mut queue) = sharedqueue.lock() {
+        let skip = if output.len() > queue.len()*channels {
+            output.len()/channels - queue.len()
+        }
+        else {
+            0
+        };
+        for frame in output.chunks_mut(channels).skip(skip) {
+            let value: T = cpal::Sample::from::<f32>(&queue.pop_front().unwrap_or(0.0));
+            for sample in frame.iter_mut() {
+                *sample = value;
+            }
         }
     }
 }
